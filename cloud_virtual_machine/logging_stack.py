075a96f0b5a5e49ca4d4484d74db6bf77a879d01
FarrOut <4e4f9ce1@opayq.com>
2021-12-21 07:59:37 +0200

    upgraded to CDK v2.2.0

--- cloud_virtual_machine/logging_stack.py
+++ cloud_virtual_machine/logging_stack.py
@@ -1,22 +1,12 @@
-from aws_cdk import (core as cdk,
-aws_ec2 as ec2,
-aws_secretsmanager as secretsmanager,
-aws_s3_assets as assets,
-aws_autoscaling as autoscaling,
-aws_logs as logs,
+import aws_cdk as cdk
+from constructs import Construct
+from aws_cdk import (
+    aws_logs as logs,
 )
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-# from pipeline_stage import WorkshopPipelineStage
-import os
 
 class LoggingStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
 # View CloudFormation Logs in the Console
