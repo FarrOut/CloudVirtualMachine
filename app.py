48d2ba5512aecc55b4ed29b20d635966798f9d9b
FarrOut <4e4f9ce1@opayq.com>
2022-07-01 16:55:32 +0200

    working on fetching list of allowed IPs from SSM parameter store

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 import os
 import socket
+from typing import List
 
 import aws_cdk as cdk
 # from cloud_virtual_machine.pipeline_stack import PipelineStack
 from aws_cdk import (
     aws_ec2 as ec2,
+    aws_ssm as ssm,
 )
 
 # from cloud_virtual_machine.logging_stack import LoggingStack
+from aws_cdk.aws_ssm import StringListParameter
+
 from cloud_virtual_machine.security import ssh_key_handler
 from cloud_virtual_machine.terminal_stack import TerminalStack
-from cloud_virtual_machine.mainframe_stack import MainframeStack
+from cloud_virtual_machine.infra_stack import InfraStack
 
 
 # TODO automatically detect local IP address
@@ -28,6 +32,10 @@ def get_my_ip() -> str:
     return ipv6
 
 
+def get_whitelisted_peers(whitelisted_peers: StringListParameter) -> List[str]:
+    return whitelisted_peers.string_list_value
+
+
 app = cdk.App()
 
 default_env = cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
@@ -37,9 +45,13 @@ the_env = rsa_env
 
 # PipelineStack(app, "PipelineStack", env=default_env)
 
-MainframeStack(app, "MainframeStack", env=the_env)
-TerminalStack(app, "TerminalStack", env=the_env, ssh_public_key_path=ssh_key_handler.generate_key_pair(),
-              whitelisted_peer=ec2.Peer.ipv4('0.0.0.0/0'))
+infra = InfraStack(app, "InfrastructureStack", env=the_env)
+
+whitelisted_peers = get_whitelisted_peers(infra.whitelisted_peers_parameter)
+
+TerminalStack(app, "TerminalStack", env=the_env, vpc=infra.vpc, ssh_public_key_path=ssh_key_handler.generate_key_pair(),
+              whitelisted_peers=whitelisted_peers, )
 # LoggingStack(app, "LoggingStack", env=euro_env)
+
 
 app.synth()
--- cloud_virtual_machine/mainframe_stack.py
+++
@@ -1,8 +0,0 @@
-import aws_cdk as cdk
-from constructs import Construct
-
-
-class MainframeStack(cdk.Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
